a02e1ca60f521cdf6f95e22a1d83f4dfbd15b36a
shrish-nitb <shrish108@gmail.com>
2024-02-24 15:30:24 +0530

    additions

---
+++ src/components/General/Glossy.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Glossy({ bg, position, children }) {
+  return (
+    <>
+      <div className="GlossyParent">
+        <div
+          className="Glossy"
+          style={{ backgroundImage: bg, backgroundPosition: position }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              alignItems: "center",
+              height: "100%",
+              paddingInline:"8.3vw"
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
