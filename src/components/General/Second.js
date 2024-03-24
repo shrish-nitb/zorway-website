06e9758889412aa7fc21e0e7afc40f0043c90016
shrish-nitb <shrish108@gmail.com>
2024-03-24 15:36:30 +0530

    fixes

--- src/components/General/Second.js
+++ src/components/General/Second.js
@@ -1,5 +1,5 @@
-import { motion, useScroll } from "framer-motion";
-import React from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useState, useEffect } from "react";
 
 export default function Second({
   purple,
@@ -10,34 +10,39 @@ export default function Second({
   destination,
 }) {
   const { scrollYProgress } = useScroll();
+  const [FSV, setFSV] = useState();
+
+  // Update FSV based on scrollYProgress
+  useEffect(() => {
+    const updateFSV = () => {
+      setFSV(floatingShineBlur.get());
+    };
+    const unsubscribe = scrollYProgress.onChange(updateFSV);
+    return () => unsubscribe();
+  }, []);
+
+  let floatingShineBlur = useTransform(scrollYProgress, [0, 0.17], [9, 0]);
+  let popAppearance = useTransform(scrollYProgress, [0.05, 0.14], [100, 0]);
+  let popzAppearance = useTransform(scrollYProgress, [0.05, 0.14], [-500, 0]);
+  let hidePop = useTransform(scrollYProgress, [0.15, 0.23], [1, 0]);
+  let anim02 = useTransform(scrollYProgress, [0, 0.14], [0, 1]);
+  let anim03 = useTransform(scrollYProgress, [0, 0.14], [0, 1.2]);
 
   return (
     <section className="home-second">
+      <motion.div className="floating-shine" style={{ filter: `blur(${FSV}px)`, opacity: hidePop }} />
       <div className="gapper" style={{ textAlign: "center" }}>
-        <div
-          style={
-            destination == "homepage" && (window.innerWidth > 639)
-              ? { height: "25rem", paddingTop: "calc(38vh - 3.8125rem)" }
-              : {}
-          }
-        >
+        <div style={destination === "homepage" && window.innerWidth > 639 ? { height: "60rem" } : {}}>
           <div className="title-white purple-head">{purple}</div>
-          <div
-            style={
-              destination == "homepage" && (window.innerWidth > 639)
-                ? { position: "sticky", top: "38vh" }
-                : {}
-            }
-          >
-            <div className="title-white">{white}</div>
-            <div className="subtitle">{subtitle}</div>
+          <div style={destination === "homepage" && window.innerWidth > 639 ? { position: "sticky", top: "308px", perspective: "1000px" } : {}}>
+            <motion.div style={{ y: popAppearance, z: popzAppearance, opacity: anim02, scale: anim03 }}>
+              <motion.div className="title-white" style={{ opacity: hidePop }}>{white}</motion.div>
+              <motion.div className="subtitle" style={{ opacity: hidePop }}>{subtitle}</motion.div>
+            </motion.div>
           </div>
         </div>
-        <div style={{overflowX:"hidden"}}>
-          <div
-            className="second-grid"
-            style={destination == "homepage" ? { paddingTop: "12.5rem" } : {}}
-          >
+        <div style={{ overflowX: "hidden" }}>
+          <div className="second-grid" style={destination === "homepage" ? { paddingTop: "12.5rem" } : {}}>
             {left}
             {right}
           </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,7 +27,7 @@ function bubbleAnimation() {
 
     const children = document.querySelectorAll('.bubble-span ');
     children.forEach((node, index) => {
-      node.style.animationDelay = `${index * .2}s`;
+      node.style.animationDelay = `${index * .1}s`;
     });
 
   });
@@ -38,48 +38,24 @@ function Home() {
 
 
   const { scrollYProgress } = useScroll();
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log(latest);
-  });
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   console.log(latest);
+  // });
 
-  let anim01 = useTransform(
-    scrollYProgress,
-    [0, 0.14140019703108825],
-    [100, 0]
-  );
-  let anim01b = useTransform(
-    scrollYProgress,
-    [0, 0.14140019703108825],
-    [100, 20]
-  );
-  let anim02 = useTransform(scrollYProgress, [0, 0.14140019703108825], [0, 1]);
-  let anim03 = useTransform(
-    scrollYProgress,
-    [0, 0.14140019703108825],
-    [0, 1.2]
-  );
-  let anim04 = useTransform(
-    scrollYProgress,
-    [0.18158116942853908, 0.27191166321601107],
-    ["100%", "0%"]
-  );
   let anim05 = useTransform(
     scrollYProgress,
-    [0.18158116942853908, 0.27191166321601107],
+    [0.18, 0.23],
     ["-100%", "0%"]
   );
   let anim06 = useTransform(
     scrollYProgress,
-    [0.18158116942853908, 0.2929223312683316],
+    [0.18, 0.23],
     [0, 1]
   );
   let anim07 = useTransform(scrollYProgress, [0.03, 0.127], [1, 0]);
   let anim08 = useTransform(scrollYProgress, [0.03, 0.127], [0, -100]);
-  let anim09 = useTransform(scrollYProgress, [0.037, 0.382], ["100%", "0%"]);
-  let anim10 = useTransform(scrollYProgress, [0.037, 0.382], ["-100%", "0%"]);
-  let anim11 = useTransform(scrollYProgress, [0.037, 0.382], [0, 1]);
 
-  const {ref, inView} = useInView();
+  const { ref, inView } = useInView();
   useEffect(() => {
     if (inView) {
       bubbleAnimation()
@@ -139,11 +115,6 @@ function Home() {
         white={
           <>
             <motion.div
-              style={
-                window.innerWidth > 850
-                  ? { y: anim01, opacity: anim02, scale: anim03 }
-                  : {}
-              }
             >
               One Organization,
               <br />
@@ -154,11 +125,7 @@ function Home() {
         subtitle={
           <>
             <motion.div
-              style={
-                window.innerWidth > 850
-                  ? { y: anim01b, opacity: anim02, scale: anim03 }
-                  : {}
-              }
+
             >
               Enable your business to create improved processes,
               <br />
@@ -224,9 +191,6 @@ function Home() {
       <section className="home-third" style={{ overflowX: "hidden" }}>
         <div className="third-flex">
           <motion.div
-            style={
-              window.innerWidth > 700 ? { x: anim10, opacity: anim11 } : {}
-            }
             className="third-flex-text"
           >
             <div>
@@ -241,7 +205,7 @@ function Home() {
               to Crafting Tailored, High-Impact Websites for Businesses of Every
               Scale, Fueling Their Online Success."
             </div>
-            <div className="outlined-btn">
+            <div className="outlined-btn" style={{ marginLeft: "unset" }}>
               <div>Get A Quote</div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -258,9 +222,6 @@ function Home() {
             </div>
           </motion.div>
           <motion.div
-            style={
-              window.innerWidth > 700 ? { x: anim09, opacity: anim11 } : {}
-            }
             className="features"
           >
             <div data-aos="zoom-out" data-aos-easing="ease-out">
@@ -380,92 +341,42 @@ function Home() {
           </motion.div>
         </div>
       </section>
-      {/* <section className="home-fourth">
-        <div className="panes-container">
-          <div className="horizontal-panes-fourth">
-            <div
-              className="cool-image"
-              style={{
-                backgroundImage:
-                  "url(https://s3-alpha-sig.figma.com/img/53a6/6956/697744e1e45002e1987f16c33d73bee5?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n1l2osIqN7ND3LCkVLL3Q1szjvFoJpp5w~6cSVK-~OF1dDS0nSr02HMUBs-NwDoLiO~9Y6uGVRMK7zi5ZvjL3xrVLn~eKTK9kGYnUruWxT3PwNp8YyHBQGErEaZIQ4s0eMuHRIgjtkLzqIsitQnamUxCTUHyjsTzNFkAM5ZEkvYih84s~tenhZEPbu5SjSRWK6vMUWyeGZsqBn-e5mpXFVF0l1RZRdRs9Xo3~hjmlLqwun5nWYa4a57UJXGM2vA8eglGTVrYLFJqIBtz1BQqaeZm4dHLQXTczhmWnZLhNAKWueWoMhu5J5EMkh2zWGFnHNKG~tIJsx0YT3e0WJ0GTw__)",
-              }}
-            ></div>
-            <div className="glass-outer glass-right">
-              <div className="glass-inner">
-                <div className="glass-text">
-                  <div>
-                    <span className="head purple-head">Web</span>
-                    <span className="head white-head"> Design</span>
-                  </div>
-                  <div className="glass-text-concept">
-                    Developing a user-friendly interface to engage more possible
-                    customers.
-                  </div>
-                </div>
-                <img
-                  className="glass-image"
-                  src="https://s3-alpha-sig.figma.com/img/7cd3/8fa5/a4bae463af518f8105d9a332943b929f?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vgcj9KTTbEWLou--g6NBt949iAGTgPEwPhJXA5qiKuXWilOSGCLIWPcsVG1sHT1bNVVnwTluU9k8a6wbPabqlowhJyo5owxxxSsXUnhC-1sIs8oC--hmqM9XH3fbQFfYNApPh1mt4NZrF1h4lmvWPcaa6zqLjOi8tnPYywHSKaNgUs1s8j6XniAvzylIyrtJ4QhMEwVGY4kMHXn0TWL00o8jgXP9bkpUp8wCXGKIJ83uRbK35g0yQDGGPscQhNm0gCcJUpKSOTnUACOY83fUNN8mmDIAs--4Q3tOlugsKcY6xNLYGC4LZxePc99~Hp-0xvxiTRPXDkn05AyNf7suNQ__"
-                ></img>
+      <section className="services-section">
+        <div>
+          <div className="title-white">
+            <span className="purple-head">What We </span>
+            Offer
+          </div>
+          <img className="curve-img" src="
+          curve.png
+          " alt="" />
+          <div className="curve">
+            <div>
+
+              <div className="service-number">1</div>
+              <div>
+                <div className="service-name">Website Development</div>
+                <div className="service-description">Developing a user-friendly interface to engage more possible customers.</div>
+              </div>
+
+            </div>
+            <div>
+              <div className="service-number">2</div>
+              <div>
+                <div className="service-name">App Development</div>
+                <div className="service-description">Developing a user-friendly interface to engage more possible customers.</div>
               </div>
             </div>
-          </div>
-          <div className="horizontal-panes-fourth horizontal-panes-fourth-reverse">
-            <div
-              className="cool-image"
-              style={{
-                backgroundImage:
-                  "url(https://s3-alpha-sig.figma.com/img/5df6/8a06/f5ce4736258eb3d116a8d861dfb9f641?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a4XoT8~f4NaLJmvEmR23qsHcxB7UljB1V9XYF6xHdWqU9jr3CJg5QAJfAASOAtA8RVIvH95LvoPAcIuMpXfn-b054jtMK9emuWZXWgnVhJxx50CzB6Tvj2k5IWkDCGSEZsCAdGa5ztPH5Fs5ntJllFF6hs2CkRLwWX6h8gpjHNHv6jIWZhXhoSgka4ieEjw5CbHvygIlgwW1i6X9OJwhh4j0bEGI1hTBXYVGoYIS4w3sEsHAeRun4PocoOrP1Zo2Z2qJL8hOLarV1qHNDoFNWezWQsO4RT1YA4~rmpHDFPypsVbeq7JWB~Rkn~G~YnlwejwUQ26-LqAeiOUpIEDEhg__)",
-              }}
-            ></div>
-            <div className="glass-outer glass-left">
-              <div className="glass-inner">
-                <div className="glass-text">
-                  <div>
-                    <span className="head purple-head">UI/UX</span>
-                    <span className="head white-head"> Development</span>
-                  </div>
-                  <div className="glass-text-concept">
-                    Magically designing logos, and other media to build the
-                    brand identity.
-                  </div>
-                </div>
-                <img
-                  className="glass-image"
-                  src="https://s3-alpha-sig.figma.com/img/3dbd/e895/b7aa68b9cc9f04e57acf88c01e57dd26?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qGAYrZyrrjxk7M7rm2-JI35bZmvywsKfu-8SNu1AHd9nwipbahNRNatFswLr6yb~GjNOf04mqTSKWb-c7BfdCau6PKxqaa78UqfsqTqPIEe3dcpSeqoZke4jIJ--Sh~KmYjXT3dO-n-EdWt4algRxo66nvc5CZDh59-f6SiX4fCYD9mGxsbNVWmQoi-SahhTkIfuAsiiAPR~kEcrCR9bQGlkFVjGJJUiO7FVEvyzNbKGJZvej1wzkFFNtiGtU0juqszbDyyVlgPVdfXg8ATyUPKv~9UaMSz1EU8hKMXW7m51UyO2-aY-8YhVlrJCUpl4qIeqPFAzxirril7VvVLM2A__"
-                ></img>
-              </div>
-            </div>
-          </div>
-          <div className="horizontal-panes-fourth">
-            <div
-              className="cool-image"
-              style={{
-                backgroundImage:
-                  "url(https://s3-alpha-sig.figma.com/img/febb/8971/b0fd508555348fe731254ea3ebd338c9?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CTmNvVvCmWfYplFQldXR5XE-zLOBgx40AM3X86n8vQbKPSga5BI3U9iYLga50s3dUU3leqJkyE7zi3JxvsEB315mGKoialW3GUegchwPvffwEyWS~jXATL9wByzln92urFoRr4IKKVoGa4jgM3Q-kWGLyTM6E49yKdqUVhmnlzqH8mF-6N4RMtmntw7tuu60ajsqMJeTxo0r9i-3oeT1XoJs5Fp2ohI86iNNtrig3vOjnKi~xMSk8LiBD7Wc4E7AA3emgJgLustyQxuq5l5~ukA3JhoqyTdyFTUhNebiH6EzolCf1~KByRqH6HpPROm0dOAZBraQiG2Q439uRBuE4w__)",
-              }}
-            ></div>
-            <div className="glass-outer glass-right">
-              <div className="glass-inner">
-                <div className="glass-text">
-                  <div>
-                    <span className="head purple-head">APP</span>
-                    <span className="head white-head"> Development</span>
-                  </div>
-                  <div className="glass-text-concept">
-                    Transforming visionary concepts into vibrant digital
-                    solutions, <br /> shaping the future of app development with
-                    every line of code we craft!
-                  </div>
-                </div>
-                <img
-                  className="glass-image"
-                  src="https://s3-alpha-sig.figma.com/img/4021/6842/3853d6fd84d5a70b86c0a56680d29c76?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=piZ8cuOLFQ0jcHaVt28AALl8Ls8z-RtydK6O4MgI6l2FOZFNt2Flu8fadN6MI195cqjBpsdtIKtSvvniirF53IdBT7KP21Gc8jMEoE7NQWf4hNo0ksXUFVjzHEUpa6rkfSkzYRlT664T-1khDk~qGqYsDPKwQKIu2oYn0EHMfTKmA1PeMu3k2GUVePh~AkUy-vC3XETEw7sLhXlZx0fHSmXfHltC~t0Sidpy2KaalhBmrXfzdkZKIUIRkykJ2iVB2fHptgQCyb4a8WTOSwUJLla4ERxiTLenqwkt51KrC~PvOsaEJAnx6-uN74maTZYqTbn7QGS6AYpIaCR9yG~oGQ__"
-                ></img>
+            <div>
+              <div className="service-number">3</div>
+              <div>
+                <div className="service-name">UI/UX Design</div>
+                <div className="service-description">Magically designing logos, and other media to build the brand identity.</div>
               </div>
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
       <section className="home-fifth">
         <div className="title-white">
           <span className="purple-head">Reasons </span>
