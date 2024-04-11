fe9f38580bdb70bcdc45a2abf7f99f8ede26f824
shrish-nitb <shrish108@gmail.com>
2024-04-11 23:59:14 +0530

    fix

--- src/components/General/Footer.js
+++ src/components/General/Footer.js
@@ -8,36 +8,48 @@ export default function Footer() {
             <div className='Maal'>
                 <div className='MaalOne'>
                     <img src="logofooter.png" className="logofooter" alt="" />
-                    <div>
-                        Get your business to the <br /> new heights.
+                    <div className="footerDesc">
+                        Take your business to new heights with our innovative solutions and strategic approaches.
                     </div>
                 </div>
-                <div className='MaalTwo'>
-                    <h1>contact@zorway.in</h1>
-                    <span>MON–FRI  9AM–6PM</span>
+                <div className='footerItemList'>
+                    <div className="footerHead">
+                        Quick link
+                    </div>
+                    <div className="footerItem">
+                        <div>Home</div>
+                        <div>Testimonial</div>
+                        <div>Blog</div>
+                        <div>About Us</div>
+                        <div>Sitemap</div>
+                    </div>
+                </div>
+                <div className='footerItemList'>
+                    <div className="footerHead">
+                        Information
+                    </div>
+                    <div className="footerItem">
+                        <div>FAQ</div>
+                        <div>Privacy Policy</div>
+                        <div>Terms & Condition</div>
+                    </div>
+                </div>
+                <div className='footerItemList'>
+                    <div className="footerHead">
+                        Contact
+                    </div>
+                    <div className="footerItem contactItem">
+                        <div>MANIT, Bhopal</div>
+                        <div>contact@zorway.in</div>
+                        <div>+91 8770741112</div>
+                    </div>
+                    <div class="social"><div><i class="fa-brands fa-twitter" aria-hidden="true"></i></div><div><i class="fa-brands fa-instagram" aria-hidden="true"></i></div><div><i class="fa-brands fa-linkedin" aria-hidden="true"></i></div></div>
                 </div>
 
-                <div className='MaalThree'>
-                    <div>
-                        <h1>+91 8770741112</h1>
-                        <span>WhatsApp or Call</span>
-                    </div>
-                    <div className="social">
-                        <div><i class="fa-brands fa-twitter"></i></div>
-                        <div><i class="fa-brands fa-instagram"></i></div>
-                        <div><i class="fa-brands fa-linkedin"></i></div>
-                    </div>
-                </div>
             </div>
             <div className='footercloser'>
-                <span className='copyright'>Copyright ©2023 Zorway. <br /> All rights reserved.</span>
-                <div className='closerlist'>
-                    <div><Link to="/">Home</Link></div>
-                    <div><Link to="/testimonial">Testimonial</Link></div>
-                    <div><Link to="/blogs">Blog</Link></div>
-                    <div><Link to="tel:+918770741112">Contact</Link></div>
-                    <div><Link to="/service" >Pricing</Link></div>
-                </div>
+                <span className='copyright'>© 2024 All rights reserved | Zorway Technologies LLP.</span>
+
             </div>
         </div>
 
